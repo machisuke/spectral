fbba1dddc58aa340661ff71ef2d24661565f0288
Jakub Rożek <jakub@rozek.tech>
2021-06-04 12:02:21 +0200

    chore: jest esm

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
-module.exports = {
-  preset: 'ts-jest',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+export default {
+  preset: 'ts-jest/presets/default-esm', // or other ESM presets
+  moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
   testMatch: ['<rootDir>/src/**/__tests__/*.(ts)'],
   testPathIgnorePatterns: ['/node_modules/', '.karma.test.ts$'],
   coveragePathIgnorePatterns: ['<rootDir>/dist/', '/node_modules/'],
   setupFilesAfterEnv: ['./setupJest.ts'],
   globalSetup: './setupJest.global.ts',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      isolatedModules: true,
+    },
+  },
 };
